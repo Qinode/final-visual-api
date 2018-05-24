7f8b6b91d635a5e923dc14d9e8551d1cbaab4856
Zheng Qin <zheng.qin29@outlook.com>
2018-05-24 15:52:13 +0100

    add data access function

---
+++ src/data_util/data.py
@@ -0,0 +1,13 @@
+from src.data_util import client
+import logging.config
+
+logging.config.fileConfig('../../logging.ini')
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
+
+
+def get_data(sensor_id, measurement, timestamp, series='winery_data'):
+    query = 'select {} from {} where sensor_id=\'{}\' and time>=\'{}\''.format(measurement, series, sensor_id, timestamp)
+    logger.info(query)
+    return client.query(query)
+
